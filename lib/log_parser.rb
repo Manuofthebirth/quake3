900bdbe9d631e396a983561d8b5eb7e1fb0f328e
Manoel do Nascimento <manu3105@yahoo.com>
2019-02-15 16:23:48 -0200

    Readable qgames.log

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,7 +1,7 @@
 require "rspec/autorun"
 
 class LogParser
-  def self.open_file(file = 'qgames.log')
+  def self.read_file(file = 'qgames.log')
     lines_array = []
     file = File.open(file, 'r')
     file.each_line { |line| lines_array << line }
@@ -10,24 +10,24 @@ class LogParser
   end
 
   def self.parse_log(file = 'qgames.log')
-    log_lines = LogParser.open_file('qgames.log')
+    log_lines = LogParser.read_file('qgames.log')
   end
 end
 
 
 describe LogParser do
   before :each do
-	@log_lines = LogParser.open_file('qgames.log')
+	  @log_lines = LogParser.read_file('qgames.log')
   end
 
-  describe '.open_file' do
+  describe '.read_file' do
     it 'should return an Array' do
 		expect(@log_lines).to be_a Array
     end
   end
 
   describe '.parse_log' do
-  	it 'can read a log file' do
+  	it 'can open a log file' do
   		expect(@log_lines).not_to be_empty
   	end
   end
