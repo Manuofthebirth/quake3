cef6d9a5f2d5ea968fedcac8430dd74fe5651cad
Manoel do Nascimento <manu3105@yahoo.com>
2019-02-17 16:57:40 -0300

    Game class created

--- lib/log_parser.rb
+++ lib/log_parser.rb
@@ -1,5 +1,3 @@
-require "rspec/autorun"
-
 class LogParser
   def self.read_file(file = 'qgames.log')
     lines_array = []
@@ -11,24 +9,5 @@ class LogParser
 
   def self.parse_log(file = 'qgames.log')
     log_lines = LogParser.read_file('qgames.log')
-  end
-end
-
-
-describe LogParser do
-  before :each do
-	  @log_lines = LogParser.read_file('qgames.log')
-  end
-
-  describe '.read_file' do
-    it 'should return an Array' do
-		expect(@log_lines).to be_a Array
-    end
-  end
-
-  describe '.parse_log' do
-  	it 'can open a log file' do
-  		expect(@log_lines).not_to be_empty
-  	end
   end
 end
