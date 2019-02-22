51bee866baa3dd7336e3d1c0632efe080c6dafac
Manoel do Nascimento <manu3105@yahoo.com>
2019-02-22 16:40:08 -0300

    Better tests for Player Class

--- lib/player.rb
+++ lib/player.rb
@@ -2,7 +2,7 @@ require_relative "log_parser"
 require_relative "game" 
 
 class Player
-  MATCHER = /\s(?<id>\d)\sn\\(?<name>\w.+)\\t/ # players_info
+  MATCHER = /\s(?<id>\d)\sn\\(?<name>[\w\s]+)\\t/ # players_info
 
   attr_reader :id
   attr_accessor :name, :kills
@@ -13,7 +13,7 @@ class Player
     @kills = 0
   end
 
-  def kill(player)
+  def add_kills(player)
     if @id.eql?(1022) # World id = 1022 ; When <world> kill the player, the player lose -1 kill score.
       player.kills -=1
     else
