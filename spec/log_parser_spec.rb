76a2ca7bb2e5b465aac8df4a5ded93e6502e1761
Manoel do Nascimento <manu3105@yahoo.com>
2019-02-21 23:22:01 -0300

    TDD for LogParser updated

--- spec/log_parser_spec.rb
+++ spec/log_parser_spec.rb
@@ -1,22 +1,35 @@
-require "log_parser"
+require 'spec_helper'
+require_relative '../lib/log_parser'
+require_relative '../lib/game'
+require 'pry-byebug'
+
 
 describe LogParser do
-  
-  before :each do
-	  @log_lines = LogParser.read_file('one_game.log')
+  subject(:log) { LogParser.new(File.open('test/test.txt').read) } # for repeated test lines
+
+  it 'should initialize with a log' do
+    expect { log }.not_to raise_error
   end
 
-  describe '.read_file' do
-    it 'can open a log file' do
-      expect(@log_lines).not_to be_empty
+  describe '#raw_log' do
+    it 'returns the raw log text' do
+      log_string = File.open('test/test.txt').read
+      expect(log.raw_log).to eq(log_string)
     end
   end
 
-
-  describe '.parse_log' do
-    it 'should return an Array of lines' do
-      expect(@log_lines).to be_a Array
+  describe '#game_logs' do
+    it 'returns separate game logs correctly' do
+      game1_string = File.open('test/game_test1.txt').read
+      game2_string = File.open('test/game_test2.txt').read
+      expect(log.game_logs[0]).to eq(game1_string)
+      expect(log.game_logs[1]).to eq(game2_string)
     end
   end
 
+  describe '#games' do
+    it 'returns an array of games' do
+      expect(log.games[0]).to be_a(Game)
+    end
+  end
 end
